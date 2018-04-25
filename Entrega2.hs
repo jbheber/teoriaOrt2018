0b3bb945490e3f463b40bf7fb9035188b8ff8932
Juan Bautista Heber <juanbheber@outlook.com>
2018-04-24 22:30:43 -0300

    Formas canonicas Expresiones

--- Entrega2.hs
+++ Entrega2.hs
@@ -4,3 +4,26 @@ data Exp = V Id | C Id | Lam [Id] Exp | App Exp [Exp] | Case Exp [Rama] | Rec Id
 type Rama = (Id, (Id, Exp))
 
 type Id = String
+
+data Val = CV Id [Val] | LamC [Id] Exp
+    deriving (Show)
+
+data Con = CC Id [Exp] | LamV [Id] Exp
+ deriving (Show)
+
+--Expresiones
+exp1::Exp
+exp1 = V "x"
+
+exp2::Exp
+exp2 = V "y"
+
+exp3::Exp
+exp3 = Case exp1 [("T", ([], C "F")), ("F", ([], exp2))]
+
+exp4::Exp
+exp4 = Lam ["x"] exp3
+
+exp5::Exp
+exp5 = Lam ["y"] exp3
+
